d120d4d89023e29a43b13cbd77f42c41630ea93b
amirsaam <amirkamangar@icloud.com>
2026-06-14 19:12:19 +0330

    feat(core): add x-bind with class/style

--- Sources/ElementaryAlpine/HTMLAttribute+Alpine.swift
+++ Sources/ElementaryAlpine/HTMLAttribute+Alpine.swift
@@ -18,4 +18,20 @@ extension HTMLAttribute.x {
     public static func show(_ value: String) -> HTMLAttribute {
         .init(name: "x-show", value: value)
     }
+
+    public static func bind(_ attribute: String, _ value: String) -> HTMLAttribute {
+        .init(name: "x-bind:\(attribute)", value: value)
+    }
+
+    public static func bind(_ value: String) -> HTMLAttribute {
+        .init(name: "x-bind", value: value)
+    }
+
+    public static func bindClass(_ value: HTMLAttributeValue.Alpine.BindClass) -> HTMLAttribute {
+        .init(name: "x-bind:class", value: value.rawValue)
+    }
+
+    public static func bindStyle(_ value: HTMLAttributeValue.Alpine.BindStyle) -> HTMLAttribute {
+        .init(name: "x-bind:style", value: value.rawValue)
+    }
 }
